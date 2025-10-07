45f685af2bf8cff8d3f4056a9397f67c2b60d139
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-07 10:30:19 +0530

    Intersection of Two Array

--- Problem_349.java
+++ Problem_349.java
@@ -2,3 +2,35 @@
 
 // 349. Intersection of Two Arrays
 
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int size=Math.min(nums1.length,nums2.length);
+        int[] ans=new int[size];
+        int k=0;
+        for(int i=0;i<nums1.length;i++){
+            boolean alreadyExist=false;
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    for(int s=0;s<k;s++){
+                        if(ans[s]==nums1[i]){
+                            alreadyExist=true;
+                            break;
+                        }
+                    }
+                    if (!alreadyExist) {
+                        ans[k++]=nums1[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        int[] finalAns=new int[k];
+        for(int i=0;i<k;i++){
+            finalAns[i]=ans[i];
+        }
+        return finalAns;
+    }
+}
