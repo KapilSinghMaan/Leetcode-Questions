e7932cb2be7f4d49b95713c4759df3272b390d63
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-07 10:35:52 +0530

    Running Sum of 1d Array

--- Problem_1480.java
+++ Problem_1480.java
@@ -3,5 +3,13 @@
 // 1480. Running Sum of 1d Array
 
 public class Problem_1480 {
-    
+    public int[] runningSum(int[] nums) {
+        int result[]=new int[nums.length];
+        result[0]=nums[0]; 
+        for(int i=1;i<nums.length;i++)
+        {
+            result[i]=result[i-1]+nums[i];
+        }
+        return result; 
+    }
 }
