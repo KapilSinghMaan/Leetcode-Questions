97c0d960dc647b50bc510d5e9356a5bf6b098774
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-08 10:24:35 +0530

    Rotate Image(Array)

--- Problem_48.java
+++ Problem_48.java
@@ -4,6 +4,28 @@
 
 public class Problem_48 {
     public void rotate(int[][] matrix) {
-        
+        int n=matrix.length;
+        for(int k=0;k<n;k++)
+        {
+            int i=0;
+            int j=n-1;
+            while (i<=j) {
+                int temp=matrix[i][k];
+                matrix[i][k]=matrix[j][k];
+                matrix[j][k]=temp;
+                i++;
+                j--;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
     }
 }
