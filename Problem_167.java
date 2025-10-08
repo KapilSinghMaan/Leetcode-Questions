adcd87493bc4d8744839ddadb1ecdbfc67a49929
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-08 10:37:06 +0530

    Two Sum II - Input Array Is Sorted

---
+++ Problem_167.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+// 167. Two Sum II - Input Array Is Sorted
+
+public class Problem_167 {
+    public int[] twoSum(int[] numbers, int target) {
+        
+    }
+}
