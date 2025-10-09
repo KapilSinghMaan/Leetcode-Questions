a73e300662e5429df49dec9417fcff020a515c83
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-09 09:46:37 +0530

    Boats to Save People

---
+++ Problem_881.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/boats-to-save-people/description/
+
+// 881. Boats to Save People
+
+public class Problem_881 {
+     public int numRescueBoats(int[] people, int limit) {
+        
+    }
+}
