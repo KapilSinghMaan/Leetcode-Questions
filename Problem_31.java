ddfc253e04cdcc2f440610b10a31ee1d9ec70ca1
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-09 10:09:21 +0530

    Next Permutation

---
+++ Problem_31.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/next-permutation/description/
+
+// 31. Next Permutation
+
+public class Problem_31 {
+    public void nextPermutation(int[] nums) {
+        
+    }
+}
