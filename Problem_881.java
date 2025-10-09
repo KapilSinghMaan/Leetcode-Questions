e37c7c57ae16defd4165d65cf7c69e5cb7bcf106
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-09 10:06:23 +0530

    Boats to Save People

--- Problem_881.java
+++ Problem_881.java
@@ -2,8 +2,23 @@
 
 // 881. Boats to Save People
 
+import java.util.Arrays;
+
 public class Problem_881 {
      public int numRescueBoats(int[] people, int limit) {
         
+        Arrays.sort(people);
+        int i=0;
+        int j=people.length-1;
+        int res=0;
+
+        while (i<=j) {
+            res++;
+            if (people[i]+people[j]<=limit) {
+                i++;
+            }
+            j--;
+        }
+        return res;
     }
 }
