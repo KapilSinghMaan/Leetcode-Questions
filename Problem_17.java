6c7cae003e8a356ed0fa83d4ecc74c91f047e811
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-13 10:55:04 +0530

    Letter Combinations of a Phone Number

--- Problem_17.java
+++ Problem_17.java
@@ -2,10 +2,36 @@
 
 // 17. Letter Combinations of a Phone Number
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem_17 {
+
+    public static String[] map={"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    public static List<String> result=new ArrayList<String>();
+
+    public static void fn(String str, int idx,String output)
+    {
+        if (idx==str.length()) {
+            result.add(output);
+            return ;
+        }
+
+        int digit=str.charAt(idx)-'0';
+        for(int k=0;k<map[digit].length();k++)
+        {
+            fn(str, idx+1, output+map[digit].charAt(k));
+        }
+
+    }
+
     public List<String> letterCombinations(String digits) {
-        
+        result.clear();
+        if (digits.length()==0) {
+            return result;
+        }
+        fn(digits, 0, "");
+        return result;
     }
 }
