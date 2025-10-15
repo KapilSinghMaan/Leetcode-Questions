1234ad79ae075ee67a2e5ca1fb99bfc47400b458
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-15 10:06:29 +0530

    Count paths

--- Extra_Questions/MazePath.java
+++ Extra_Questions/MazePath.java
@@ -5,5 +5,24 @@
 package Extra_Questions;
 
 public class MazePath {
-    
+    public static int count=0;
+    public static void fn(int i,int j,int m,int n,String output)
+    {
+        if (i==m-1 && j==n-1) {
+            System.out.println(output); // Print a particular path
+            count++;
+            return;
+        }
+        if (i>=m|| j>=n) {
+            return;
+        }
+        fn(i+1, j, m, n, output+"D");
+        fn(i, j+1, m, n, output+"R");
+    }
+    public static void main(String[] args) {
+        count=0;
+        int m=3,n=3;
+        fn(0, 0, m, n, "");
+        System.out.println(count); // Print number of total paths
+    }
 }
