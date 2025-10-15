284a53e5f1f3e40c6382a19cd52637765725da02
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-15 09:32:39 +0530

    Lexicographical Numbers

---
+++ Problem_386.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/lexicographical-numbers/description/
+
+// 386. Lexicographical Numbers
+
+import java.util.List;
+
+public class Problem_386 {
+
+    public List<Integer> lexicalOrder(int n) {
+        
+    }
+}
