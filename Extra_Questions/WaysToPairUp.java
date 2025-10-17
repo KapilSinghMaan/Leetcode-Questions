8f608c71256049706ef41ea6af4fa5da622edef0
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-17 13:56:55 +0530

    Number of pairs

---
+++ Extra_Questions/WaysToPairUp.java
@@ -0,0 +1,8 @@
+// Given 'n' friends, each one can remain single or can be paires up with some other friend. Each friend can be paired only once. Find out the total number of ways in which friends can remain single or can be paired up.
+
+// Example--> Input: n=3 Output: 4
+
+public class WaysToPairUp {
+
+    
+}
