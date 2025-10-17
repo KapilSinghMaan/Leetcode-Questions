618acfc808d5c95bfa0a3710debf9b5f70ae0539
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-17 14:03:52 +0530

    Number of pairs

--- Extra_Questions/WaysToPairUp.java
+++ Extra_Questions/WaysToPairUp.java
@@ -2,7 +2,25 @@
 
 // Example--> Input: n=3 Output: 4
 
+import java.util.Scanner;
+
 public class WaysToPairUp {
 
-    
+    public static int fn(int n)
+    {
+        if (n==1) {
+            return 1;
+        }
+        else if(n==2){
+            return 2;
+        }
+        return fn(n-1) +(n-1)*fn(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter Number of Friends: ");
+        int n=scanner.nextInt();
+        System.out.println(fn(n));
+        scanner.close();
+    }
 }
