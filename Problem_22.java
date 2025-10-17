c147ce3268ca3a69e53fbfa872e9b6f512e9ce15
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-17 14:31:15 +0530

    Generate Parentheses

---
+++ Problem_22.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/generate-parentheses/description/
+
+// 22. Generate Parentheses
+
+import java.util.List;
+
+public class Problem_22 {
+    public List<String> generateParenthesis(int n) {
+        
+    }
+}
