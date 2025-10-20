ccb520ec9ea0f37d860103bb0ccb7bbaa9aabebd
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-20 13:21:21 +0530

    Maze Path

--- Extra_Questions/MazePath.java
+++ Extra_Questions/MazePath.java
@@ -1,4 +1,4 @@
-// Given two numbers (m,n) representing dimensions of a 'm x n' board. You arestanding at top left of this board and want to reach the bottom right. In how many ways you can reach the bottom right from the top left if from every cell you can go either to rightward cell or downward cell.
+// Given two numbers (m,n) representing dimensions of a 'm x n' board. You are standing at top left of this board and want to reach the bottom right. In how many ways you can reach the bottom right from the top left if from every cell you can go either to rightward cell or downward cell.
 
 // Example: for m=3 and n=3 there are 6 possible paths.
 
