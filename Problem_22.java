8c571d9891858148606422d3c808c5fff75d5480
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-20 13:20:00 +0530

    Generate Parentheses

--- Problem_22.java
+++ Problem_22.java
@@ -2,10 +2,28 @@
 
 // 22. Generate Parentheses
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem_22 {
-    public List<String> generateParenthesis(int n) {
-        
+
+    public static List<String> result = new ArrayList<>();
+
+    public static void fn(int n,int open,int close,String output){
+        if (close==n) {
+            result.add(output);
+            return;
+        }
+        if (open>close) {
+            fn(n, open, close+1,output+")");
+        }
+        if (open<n) {
+            fn(n, open+1, close,output+"(");
+        }
+    }
+    public static List<String> generateParenthesis(int n) {
+        result.clear();
+        fn(n, 0, 0,"");
+        return result;
     }
 }
