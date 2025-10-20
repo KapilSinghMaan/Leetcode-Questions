4062358a589bd4538abf3eb58bd0d339b1ed55dd
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-20 14:15:11 +0530

    Message Decoding

---
+++ Extra_Questions/DecodeMessage.java
@@ -0,0 +1,23 @@
+// Alice and Bob need to send secret messages to each other and are discussing ways to encode their messages:
+
+// ALICE :  "Let's just use a very simple code: We'll assign 'A' the codeword 1, 'B' will be 2, and so on down to 'Z' beig assigned 26."
+
+// BOB : "That's a stupid code, Alice. Suppose I send you the word 'BEAN' encoded as 25114. You could decode that in many different ways!"
+
+// ALICE : "Sure you could, but what words would you get? Other than 'BEAN' you'd get 'BEAAD', 'YAAD', 'YAN', 'YKD' and 'BEKD'. I think you would be able to figure out the correct decoding. And why would you send me the word 'BEAN' anyway?"
+
+// BOB : "OK, maybe that's a bad example, but I bet you that if you got a string of length 5000 there would be tons of different decodings and with that many you would find at least two different ones that would make sense."
+
+// ALICE : "How many different decodings?"
+
+// BOB : "Jillions!"
+
+// For some reason, Alice is still unconvinced by Bob's argument, so she requires a program that will determine how many decodings there can be for a given string using her code.
+
+// For each input set, print all possible decodings for the input string.
+
+public class DecodeMessage {
+    public static void main(String[] args) {
+        
+    }
+}
