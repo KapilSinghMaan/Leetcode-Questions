4cf72248fa38158eb63e6e326f65f00271c16b51
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-21 22:33:08 +0530

    Frog jumping stones with heights

---
+++ Extra_Questions/FrogJumpingStones.java
@@ -0,0 +1,13 @@
+// There are N stones, numbered 1,2,3......,N. For each i(1<=i<=N), the height of Stone 'i' is 'hi'(height of ith stone).
+
+// There is a frog who is initially on Stone 1. He will repeat the following action some number of times to reach Stone N :
+//      --> If the frog is currently on Stone i, jump to Stone (i+1), or Stone (i+2). Here, a cost of | hi - hj | is incurred, where j is the stone to land on.
+
+// Find the minimum possible total cost incurred before the frog reaches Stone N.
+
+public class FrogJumpingStones {
+
+    public static void main(String[] args) {
+        
+    }
+}
