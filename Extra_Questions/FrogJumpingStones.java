6460b0eefd2503f42a96cbf787679ebec30085fb
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-21 23:19:17 +0530

    Frog jumping stones with heights

--- Extra_Questions/FrogJumpingStones.java
+++ Extra_Questions/FrogJumpingStones.java
@@ -5,9 +5,32 @@
 
 // Find the minimum possible total cost incurred before the frog reaches Stone N.
 
+// Example --> h[]= { 10, 20, 30, 40 }; then minimum cost = 30
+
 public class FrogJumpingStones {
 
+    static int h[] = { 10, 20, 30, 40 };
+
+    public static int fn(int N, int i) {
+
+        if (i == N) {
+            return 0;
+        }
+        if (i > N) {
+            return Integer.MAX_VALUE;
+        }
+        int nextstone = Integer.MAX_VALUE;
+        int nextnextstone = Integer.MAX_VALUE;
+        if (i + 1 < h.length) {
+            nextstone = Math.abs(h[i] - h[i + 1]) + fn(N, i + 1);
+        }
+        if (i + 2 < h.length) {
+            nextnextstone = Math.abs(h[i] - h[i + 2]) + fn(N, i + 2);
+        }
+        return Math.min(nextstone, nextnextstone);
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("Minimum cost : " + fn(h.length-1, 0));
     }
 }
