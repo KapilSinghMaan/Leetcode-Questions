117430cd2ae358643691ce8270ff8b2a8c6c8894
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-24 23:32:25 +0530

    Palindrome Number

--- Problem_9.java
+++ Problem_9.java
@@ -3,5 +3,21 @@
 // 9. Palindrome Number
 
 public class Problem_9 {
-   
+    public boolean isPalindrome(int x) {
+        int newNum = 0, num = x;
+        if (x < 0) {
+            return false;
+        } else {
+            while (x != 0) {
+                int a = x % 10;
+                newNum = newNum * 10 + a;
+                x = x / 10;
+            }
+            if (newNum == num) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
