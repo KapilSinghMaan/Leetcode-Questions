2d4e3f9eb0a462a27721b2f8a6791d81298cbae0
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-25 23:11:51 +0530

    Selection Sort

--- Extra_Questions/SelectionSort.java
+++ Extra_Questions/SelectionSort.java
@@ -4,5 +4,32 @@
 
 public class SelectionSort {
 
+    public static int findMinIndex(int[] arr, int start){
+        int minIndex=start;
+        for (int i = minIndex; i < arr.length; i++) {
+            if (arr[i]<arr[minIndex]) {
+                minIndex=i;
+            }
+        }
+        return minIndex;
+    }
+ 
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex=findMinIndex(arr,i);
+            if (minIndex!=i) {
+                int temp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={15,2,4,1,23,12,2,4,11};
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
     
 }
