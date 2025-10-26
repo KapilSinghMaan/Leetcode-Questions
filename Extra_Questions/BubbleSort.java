291161aed87ee616fda53749c8a7d8a93c1734ae
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-26 23:25:04 +0530

    Bubble Sort

---
+++ Extra_Questions/BubbleSort.java
@@ -0,0 +1,7 @@
+// Bubble Sort
+
+// Bubble Sort is a simple comparison-based sorting algorithm that works by repeatedly stepping through the list, comparing adjacent elements, and swapping them if they are in the wrong order.
+
+class BubbleSort{
+    
+}
