92e193c2ff28775ef1df815c73c7708a412a7111
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-27 10:36:51 +0530

    Merging Two Sorted Arrays of different lengths

--- Extra_Questions/MergeTwoSortedArrays.java
+++ Extra_Questions/MergeTwoSortedArrays.java
@@ -2,5 +2,35 @@
 
 public class MergeTwoSortedArrays {
 
-    
+    public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
+        int n = a.length;
+        int m = b.length;
+        int[] arr = new int[n + m];
+        int i = 0, j = 0, k = 0;
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                arr[k++] = a[i];
+                i++;
+            } else {
+                arr[k++] = b[j];
+                j++;
+            }
+        }
+        while (i < n) {
+            arr[k++] = a[i++];
+        }
+        while (j < m) {
+            arr[k++] = b[j++];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 2, 4, 6, 8, 10 };
+        int[] b = { 1, 3, 5, 7, 9, 11, 13, 21 };
+        int[] arr = mergeTwoSortedArrays(a, b);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
