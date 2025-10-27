1d489129ef107516d88d377c41d8d1ade491bf05
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-27 10:41:52 +0530

    Merge Sort

---
+++ Extra_Questions/MergeSort.java
@@ -0,0 +1,7 @@
+// Merge Sort
+
+// Merge Sort is a divide-and-conquer algorithm that splits an array into halves, recursively sorts each half, and then merges them back together in sorted order. It has a consistent time complexity of O(n log n) and is known for being stable and efficient, especially with large datasets.
+
+public class MergeSort {
+    
+}
