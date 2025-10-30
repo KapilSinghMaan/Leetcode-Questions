55831e9fe8cdf54dff07e803a94856eb6fe161b1
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-30 21:34:38 +0530

    Merge Sort

--- Extra_Questions/MergeSort.java
+++ Extra_Questions/MergeSort.java
@@ -4,5 +4,68 @@
 
 public class MergeSort {
 
-    
+    public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
+        int n = a.length;
+        int m = b.length;
+        int[] ar = new int[n + m];
+        int i = 0, j = 0, k = 0;
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                ar[k++] = a[i];
+                i++;
+            } else {
+                ar[k++] = b[j];
+                j++;
+            }
+        }
+        while (i < n) {
+            ar[k++] = a[i++];
+        }
+        while (j < m) {
+            ar[k++] = b[j++];
+        }
+        return ar;
+    }
+
+    public static void merge(int[] arr, int start, int mid, int end) {
+        int[] left = new int[mid - start + 1];
+        int[] right = new int[end - mid];
+        int k = 0;
+        for (int i = start; i <= mid; i++) {
+            left[k++] = arr[i];
+        }
+        k = 0;
+        for (int i = mid + 1; i <= end; i++) {
+            right[k++] = arr[i];
+        }
+
+        int[] result = mergeTwoSortedArrays(left, right);
+
+        k = 0;
+        for (int i = start; i <= end; i++) {
+            arr[i] = result[k++];
+        }
+    }
+
+    public static void f(int[] arr, int start, int end) {
+        if (end <= start) {
+            return;
+        }
+        int mid = (start + end) / 2;
+        f(arr, start, mid);
+        f(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+
+    public static void mergeSort(int[] arr) {
+        f(arr, 0, arr.length - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, -2, 23, 2, 45, 6, 10, 4 };
+        mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
