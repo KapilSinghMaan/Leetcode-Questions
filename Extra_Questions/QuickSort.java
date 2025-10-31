c038ed2da02121fe237979a5a27cbd1d1d8098db
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-01 00:50:37 +0530

    Quick Sort

---
+++ Extra_Questions/QuickSort.java
@@ -0,0 +1,9 @@
+// Quick Sort
+
+// Quick Sort is a divide-and-conquer sorting algorithm that selects a pivot element and partitions the array around it, recursively sorting the subarrays.
+
+public class QuickSort {
+    public static void main(String[] args) {
+        
+    }
+}
