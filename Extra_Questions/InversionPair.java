2e7a22693f10e97317cf64f376a7ed53c28d3b20
KapilSinghMaan <kapilmaan369@gmail.com>
2025-10-31 21:45:39 +0530

    Inversion Pair

---
+++ Extra_Questions/InversionPair.java
@@ -0,0 +1,8 @@
+// Inversion Pair
+
+// Let A[0,1,2,3.....n-1] be an array of n distinct positive integers. If i < j and  A[i] > A[j] then the pair (i,j) is called an inversion of A. Given n and an array A, your task is to find the number of inversions of A.
+
+public class InversionPair {
+
+    
+}
