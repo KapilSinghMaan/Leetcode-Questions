b7516c84a85e00006581b36f66a761f0b6a95cc7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 15:53:20 +0530

    Find Peak Element

---
+++ Problem_162.java
@@ -0,0 +1,9 @@
+// 162. Find Peak Element
+
+// https://leetcode.com/problems/find-peak-element/description/
+
+public class Problem_162 {
+    public int findPeakElement(int[] nums) {
+        
+    }
+}
