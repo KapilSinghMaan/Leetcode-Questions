52f4e5a6ea8744934f89588038a7812866237959
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 22:40:08 +0530

    Aggressive Cows (Binary Search Problem)

---
+++ Extra_Questions/AggressiveCows.java
@@ -0,0 +1,14 @@
+// Aggressive Cows (Binary Search Problem)
+
+// Farmer Jhon has built a new long barn, with N stalls. The stalls are located along a straight line at positions x1,x2,x3.....xN.
+
+// His C cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, Jhon wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+
+// Example : no. of cows = 3
+// position of stall = (1,2,8,4,9)
+
+// Output : Minimum distance : 3
+
+public class AggressiveCows {
+    
+}
