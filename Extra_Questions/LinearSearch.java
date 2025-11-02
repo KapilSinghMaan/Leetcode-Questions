85d8b4bc3e42ef95c3de394784ede6b6f778e014
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 15:31:36 +0530

    Linear Search

---
+++ Extra_Questions/LinearSearch.java
@@ -0,0 +1,7 @@
+// Linear Search
+
+// Linear Search is a simple search algorithm that checks each element in a list sequentially until the desired value is found or the end of the list is reached.
+
+public class LinearSearch {
+    
+}
