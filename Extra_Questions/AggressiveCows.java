cf4212c3d2b8af64c4b331343ee64f92392d024a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 22:55:59 +0530

    Aggressive Cows (Binary Search Problem)

--- Extra_Questions/AggressiveCows.java
+++ Extra_Questions/AggressiveCows.java
@@ -9,6 +9,57 @@
 
 // Output : Minimum distance : 3
 
+import java.util.Arrays;
+
 public class AggressiveCows {
-    
+
+    public static int distributedCows(int[] arr,int n,int c){
+
+        Arrays.sort(arr);
+        int low=1;
+        int max=0;
+
+        for (int i : arr) {
+            max=Math.max(max, i);
+        }
+
+        int high=max;
+        int ans=0;
+
+        while (low<=high) {
+            int mid=(low+high)/2;
+
+            if (good(arr,mid,n,c)) {
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public static boolean good(int[] arr,int mid,int n,int c){
+
+        int count=1;
+        int last_pos=arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]-last_pos>=mid) {
+                last_pos=arr[i];
+                count++;
+            }
+            if (count==c) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int c=3;
+        int[] arr={1,2,8,4,9};
+
+        System.out.println("Minimum distance : "+ distributedCows(arr,n,c));
+    }
 }
