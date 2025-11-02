449deaaba14b58fe7f87a98969421a080d06984b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 00:10:08 +0530

    Search in Rotated Sorted Array

--- Problem_33.java
+++ Problem_33.java
@@ -4,6 +4,35 @@
 
 public class Problem_33 {
     public int search(int[] nums, int target) {
-        
+
+        int low=0;
+        int high=nums.length-1;
+
+        while (low<=high) {
+            int mid=(low+high)/2;
+
+            if (nums[mid]==target) {
+                return mid;
+            }
+            if (nums[0]<=nums[mid]) {
+                
+                if (nums[0]<= target && nums[mid]>target) {
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if (nums[mid]<target && nums[high]>=target) {
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
