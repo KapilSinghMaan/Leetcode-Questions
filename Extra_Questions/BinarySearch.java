82d2505cb8fce5aea701192e7c92de48660a0621
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 15:38:39 +0530

    Binary Search

---
+++ Extra_Questions/BinarySearch.java
@@ -0,0 +1,7 @@
+// Binary Search
+
+// Binary Search is a fast and efficient algorithm used to find the position of a target value within a sorted array by repeatedly dividing the search interval in half.
+
+public class BinarySearch {
+    
+}
