da34ea36e85de1bf3bdf7f7f13d01d9d2d704e3d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 21:21:41 +0530

    Very Easy Task (Binary Search Problem)

---
+++ Extra_Questions/VeryEasyTask.java
@@ -0,0 +1,17 @@
+// Very Easy Task (Binary Search to Answer)
+
+// This morning the jury decided to add one more ,Very Easy Problem to the problem set of the olympiad. The executive secretary of the Organizing Committee has printed its statement in one copy, and now they need to make n more copies before the start of the olympiad.
+// They have two copies at his disposal, one of which copies a sheet in x seconds, and the other in y seconds.
+// It is allowed to use one copier or both at the same time. You can copy not only from the original, but also from the copy.
+// Help them find out what is the minimum time they need to make 'n' copies of the statement.
+
+// Input --> The program receives three integers : n , x, y 
+// Output -> Print one integer, the minimum time in seconds required to get 'n' copies.
+
+// Example: input--> 4 1 1      output --> 3
+// Example: input--> 5 1 2      output --> 4
+
+public class VeryEasyTask {
+
+    
+}
