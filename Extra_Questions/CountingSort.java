235a82718cca455816888f49938629520ba073ce
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-02 15:28:51 +0530

    Counting Sort

--- Extra_Questions/CountingSort.java
+++ Extra_Questions/CountingSort.java
@@ -4,5 +4,34 @@
 
 public class CountingSort {
 
+    public static int[] countingSort(int[] arr){
+        int max=Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i>max) {
+                max=i;
+            }
+        }
+        int[] result=new int[arr.length];
+        int[] fre=new int[max+1];
+
+        for (int i = 0; i < arr.length; i++) {
+                fre[arr[i]]+=1;
+        }
+        for (int i = 1; i < fre.length; i++) {
+            fre[i]+=fre[i-1];
+        }
+
+        for (int i = arr.length-1; i >=0; i--) {
+            result[--fre[arr[i]]]=arr[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr={3,2,1,4,3,2,5,4,2,1,0,4};
+        int[] output=countingSort(arr);
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i]+" ");
+        }
+    }
     
 }
