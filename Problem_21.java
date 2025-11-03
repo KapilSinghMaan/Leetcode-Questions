7bc3f6f12f3ba9c90bd99df12a0f466614693344
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:21:03 +0530

    Merge Two Sorted List

---
+++ Problem_21.java
@@ -0,0 +1,26 @@
+// 21. Merge Two Sorted Lists
+
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+}
