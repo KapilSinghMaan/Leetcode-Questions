203e0ce33e15b305ec3053e2ea886513edc379f4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:10:09 +0530

    Middle of the Linked List

--- Problem_876.java
+++ Problem_876.java
@@ -21,6 +21,15 @@ class ListNode {
 
 public class Problem_876 {
     public ListNode middleNode(ListNode head) {
-        
-    }   
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 }
