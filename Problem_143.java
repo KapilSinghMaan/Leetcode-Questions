9418af8fbbee249e80b943df678dc140b0fee984
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 20:39:25 +0530

    Reorder List

--- Problem_143.java
+++ Problem_143.java
@@ -20,7 +20,54 @@ class ListNode {
 }
 
 public class Problem_143 {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
      public void reorderList(ListNode head) {
-        
+
+        if (head==null || head.next==null) {
+            return;
+        }
+
+        ListNode mid=middleNode(head);
+        ListNode second=reverseList(mid.next);
+        mid.next=null;
+        ListNode first=head;
+        while (second!=null) {
+            ListNode temp1=first.next;
+            ListNode temp2=second.next;
+            
+            first.next=second;
+            second.next=temp1;
+
+            first=temp1;
+            second=temp2;
+        }
     }
 }
