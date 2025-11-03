5fb4c001ea8962aacf551fb3ee9fbda882fb41ae
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:45:52 +0530

    Intersection of Two Linked Lists

---
+++ Problem_160.java
@@ -0,0 +1,26 @@
+// 160. Intersection of Two Linked Lists
+
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_160 {
+     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+    }
+}
