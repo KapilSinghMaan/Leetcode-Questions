213035b503beda54658bb0253a40ebaa129d17a5
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:07:33 +0530

    Middle of the Linked List

---
+++ Problem_876.java
@@ -0,0 +1,26 @@
+// 876. Middle of the Linked List
+
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_876 {
+    public ListNode middleNode(ListNode head) {
+        
+    }   
+}
