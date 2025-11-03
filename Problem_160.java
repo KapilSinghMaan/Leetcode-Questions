79cbc508140be08169f9af45753424fdc18345de
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:59:12 +0530

    Intersection of Two Linked Lists

--- Problem_160.java
+++ Problem_160.java
@@ -20,7 +20,39 @@ class ListNode {
 }
 
 public class Problem_160 {
+
+    public static int getListLength(ListNode head){
+        ListNode temp=head;
+        int count=0;
+        while (temp!=null) {
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+
      public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+        int lA=getListLength(headA);
+        int lB=getListLength(headB);
+
+        while (lA>lB) {
+            lA--;
+            headA=headA.next;
+        }
+        while (lB>lA) {
+            lB--;
+            headB=headB.next;
+        }
+
+        while (headA!=null&& headB!=null) {
+            
+            if (headA==headB) {
+                return headA;
+            }
+
+            headA=headA.next;
+            headB=headB.next;
+        }
+        return null;
     }
 }
