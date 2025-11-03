ba26f8b3f4b00db96d3e458973516b7ca2fbdd7f
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 10:18:30 +0530

    Linked List Insertion Deletion

---
+++ Extra_Questions/LinkedListInsertionDeletion.java
@@ -0,0 +1,8 @@
+// Linked List
+
+// A linked list is a linear data structure where elements (called nodes) are connected using pointers, allowing dynamic memory allocation and efficient insertions/deletions.
+
+public class LinkedListInsertionDeletion {
+
+    
+}
