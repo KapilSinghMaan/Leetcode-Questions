ee9dbfa674932698d8ca5b45479a09da6bceac76
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 19:05:33 +0530

    Reverse Linked List

--- Problem_206.java
+++ Problem_206.java
@@ -2,7 +2,7 @@
 
 // https://leetcode.com/problems/reverse-linked-list/description/
 
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
 
@@ -22,5 +22,16 @@ public class ListNode {
 public class Problem_206 {
     public ListNode reverseList(ListNode head) {
 
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 }
