6a7c1a158141121ce76fa9e52aa993206cca0871
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-03 20:11:20 +0530

    Reorder List

---
+++ Problem_143.java
@@ -0,0 +1,26 @@
+// 143. Reorder List
+
+// https://leetcode.com/problems/reorder-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_143 {
+     public void reorderList(ListNode head) {
+        
+    }
+}
