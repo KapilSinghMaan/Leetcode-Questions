3b0fd024f92ba3f1d1e83259a6d5f473986077ff
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-04 23:42:22 +0530

    Linked List - Reverse Nodes in k-Group

---
+++ Problem_25.java
@@ -0,0 +1,19 @@
+// 25. Reverse Nodes in k-Group
+
+// https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+    }
+}
