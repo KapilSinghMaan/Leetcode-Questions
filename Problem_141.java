38bafa6d0c9379729ad6a217acd71ef2cdfc6e0c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-04 23:01:02 +0530

    Linked List Cycle

---
+++ Problem_141.java
@@ -0,0 +1,20 @@
+// 141. Linked List Cycle
+
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_141 {
+
+    public boolean hasCycle(ListNode head) {
+        
+    }
+}
