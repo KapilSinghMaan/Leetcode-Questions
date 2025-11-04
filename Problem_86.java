47c2abb6de877202210a90473728d6ec4fecdf94
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-05 01:06:17 +0530

    Linked List - Partition List

---
+++ Problem_86.java
@@ -0,0 +1,19 @@
+// 86. Partition List
+
+// https://leetcode.com/problems/partition-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_86 {
+    public ListNode partition(ListNode head, int x) {
+        
+    }   
+}
