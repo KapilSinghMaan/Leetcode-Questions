b09983d278d159e972fca2db0658738fc1db6ad4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-04 23:17:40 +0530

    Linked List Cycle

--- Problem_141.java
+++ Problem_141.java
@@ -15,6 +15,22 @@ class ListNode {
 public class Problem_141 {
 
     public boolean hasCycle(ListNode head) {
-        
+
+        if (head==null) {
+            return false;
+        }
+
+        ListNode slow=head;
+        ListNode fast=head.next;
+
+        while (fast!=slow) {
+            if (fast== null || fast.next==null) {
+                return false;
+            }
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return true;
     }
 }
