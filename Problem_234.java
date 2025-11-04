da99d63df4be8a034de3349f0501a2b8c6c4293b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-04 23:39:06 +0530

    Pallindrome Linked List

--- Problem_234.java
+++ Problem_234.java
@@ -13,7 +13,52 @@ class ListNode {
 }
 
 public class Problem_234 {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
     public boolean isPalindrome(ListNode head) {
 
+            if (head==null || head.next==null) {
+                return true;
+            }
+
+            ListNode first=head;
+            ListNode second=reverseList(middleNode(head));
+
+            while (second!=null) {
+                if (first.val!=second.val) {
+                    return false;
+                }
+                first=first.next;
+                second=second.next;
+            }
+
+            return true;
     }
 }
