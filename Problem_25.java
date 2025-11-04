d65964a8eb82b63951505edec03c277f745c8ce4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-05 00:32:35 +0530

    Linked List - Reverse Nodes in k-Group

--- Problem_25.java
+++ Problem_25.java
@@ -13,7 +13,36 @@ class ListNode {
 }
 
 public class Problem_25 {
+
+    public ListNode reverseList(ListNode head, int k) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (k-- > 0 && curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
     public ListNode reverseKGroup(ListNode head, int k) {
-        
+
+        ListNode temp = head;
+        ListNode newHead = null;
+        int count = 0;
+        while (temp != null && count < k) {
+            count++;
+            temp = temp.next;
+        }
+        if (count < k) {
+            return head;
+        }
+        newHead = reverseList(head, k);
+        head.next = reverseKGroup(temp, k);
+        return newHead;
     }
 }
