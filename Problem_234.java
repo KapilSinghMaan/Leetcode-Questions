5202061a642e673d7c4bf0bb0d21c1633008102e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-04 23:27:59 +0530

    Pallindrome Linked List

---
+++ Problem_234.java
@@ -0,0 +1,19 @@
+// 234. Palindrome Linked List
+
+// https://leetcode.com/problems/palindrome-linked-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_234 {
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
