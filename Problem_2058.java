46e8542934d4a34658230b7548ee977bbecd66b9
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-05 19:55:04 +0530

    Linked List - Find the Minimum and Maximum Number of Nodes Between Critical Points

--- Problem_2058.java
+++ Problem_2058.java
@@ -13,7 +13,51 @@ class ListNode {
 }
 
 public class Problem_2058 {
-     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        
+
+    public boolean isCritialPoint(ListNode prev, ListNode curr) {
+        return (curr.val > prev.val && curr.val > curr.next.val) || (curr.val < prev.val && curr.val < curr.next.val);
+    }
+
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+
+        if (head == null || head.next == null || head.next.next == null) {
+            return new int[] { -1, -1 };
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        int count = 0;
+        int first = -1;
+        int last = -1;
+        int idx = 1;
+
+        int minDis = Integer.MAX_VALUE;
+        int maxDis = Integer.MIN_VALUE;
+
+        while (curr.next != null) {
+
+            if (isCritialPoint(prev, curr)) {
+                count++;
+                if (count == 1) {
+                    first = idx;
+                    last = idx;
+                } else {
+                    minDis = Math.min(minDis, idx - last);
+                    last = idx;
+                }
+            }
+            idx++;
+            prev = prev.next;
+            curr = curr.next;
+        }
+
+        if (count == 1 || count == 0) {
+            return new int[] { -1, -1 };
+        }
+
+        maxDis = Math.max(maxDis, last - first);
+
+        return new int[] { minDis, maxDis };
     }
 }
