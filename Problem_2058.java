a9b31a4d7f5539dbf0d4935d659ebd50be1752ba
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-05 19:28:03 +0530

    Linked List - Find the Minimum and Maximum Number of Nodes Between Critical Points

---
+++ Problem_2058.java
@@ -0,0 +1,19 @@
+// 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points
+
+// https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_2058 {
+     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+    }
+}
