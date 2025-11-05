850115574c51afa510242496ec07d2d80d6f0530
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-05 19:06:31 +0530

    Maximum Twin Sum of a Linked List

---
+++ Problem_2130.java
@@ -0,0 +1,9 @@
+// 2130. Maximum Twin Sum of a Linked Lis
+
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
+
+public class Problem_2130 {
+    public int pairSum(ListNode head) {
+
+    }
+}
