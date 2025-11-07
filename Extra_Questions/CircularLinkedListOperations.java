3502089a682e1743db7d91d9e96a52518ab86efc
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-07 22:45:11 +0530

    Circular Linked List Operations

--- Extra_Questions/CircularLinkedListOperations.java
+++ Extra_Questions/CircularLinkedListOperations.java
@@ -1,3 +1,5 @@
+// A circular linked list is a variation of a linked list where the last node points back to the first node, forming a continuous loop
+
 class CircularLinkedListOperations{
     
 }
