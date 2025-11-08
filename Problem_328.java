b78d02e288bcb3a7358b2fc8746f73d3cf4b12b6
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-09 00:00:28 +0530

    Odd Even Linked Listt

---
+++ Problem_328.java
@@ -0,0 +1,28 @@
+// 328. Odd Even Linked List
+
+// https://leetcode.com/problems/odd-even-linked-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_328 {
+
+    public ListNode oddEvenList(ListNode head) {
+
+    }
+
+}
