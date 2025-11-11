eaf8bf9f4ce13ac356a01bb6cdea063679fb341c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-12 00:07:50 +0530

    Sum Pair in Doubly Linked List

--- Extra_Questions/PairSumDoublyLL.java
+++ Extra_Questions/PairSumDoublyLL.java
@@ -10,6 +10,38 @@
 
 public class PairSumDoublyLL {
 
-    
+    class Node {
+        int data;
+        Node next, prev;
+    }
+
+    public static void pairSum(Node head, int x) {
+        Node first = head;
+        Node second = head;
+
+        while (second.next != null) {
+            second = second.next;
+        }
+
+        boolean found = false;
+        while (first != second && second.next != first) {
+            if ((first.data + second.data) == x) {
+                found = true;
+                System.out.println("(" + first.data + " , " + second.data + ")");
+                first = first.next;
+                second = second.prev;
+            } else {
+                if ((first.data + second.data) < x) {
+                    first = first.next;
+                } else {
+                    second = second.prev;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found");
+        }
+    }
 
 }
