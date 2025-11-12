362609f2a754b938174b22a5d964eba6e2ceff22
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-12 23:02:37 +0530

    Sum Pair in Doubly Linked List

--- Extra_Questions/PairSumDoublyLL.java
+++ Extra_Questions/PairSumDoublyLL.java
@@ -2,7 +2,7 @@
 
 // Given a sorted doubly linked list of positive distinct elements, the task is to find the pairs in a doubly linked list whose sum is equal to the value x , without using any extra space?
 
-// Input : head : 1<-->2<-->4<-->5<-->6<-->8<-->9
+// Input : head : 1 <--> 2 <--> 4 <--> 5 <--> 6 <--> 8 <--> 9
 //                x = 7
 // Output : (6,1) , (5,2)
 
