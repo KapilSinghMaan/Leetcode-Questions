b21c361666a4a85d3c3fa0d926d259c770f2dcf5
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-13 23:48:50 +0530

    Linked List - Merge Nodes in Between Zeros

--- Problem_2181.java
+++ Problem_2181.java
@@ -14,6 +14,27 @@ class ListNode {
 
 public class Problem_2181 {
     public ListNode mergeNodes(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
 
+        ListNode newNode =new ListNode(0);
+
+        ListNode newTemp=newNode;
+        ListNode newHead=newNode;
+
+        int sum=0;
+        ListNode temp=head.next;
+
+        while (temp!=null) {
+            sum+=temp.val;
+            if (temp.val==0) {
+                ListNode sumNode=new ListNode(sum);
+                newTemp.next=sumNode;
+                newTemp=newTemp.next;
+                sum=0;
+            }
+            temp=temp.next;
+        }
+        return newHead.next;
     }
 }
