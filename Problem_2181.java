3c0c69b9d099d84ad72057bd7cd69cf1c4becf89
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-13 23:42:58 +0530

    Linked List - Merge Nodes in Between Zeros

---
+++ Problem_2181.java
@@ -0,0 +1,19 @@
+// 2181. Merge Nodes in Between Zeros
+
+// https://leetcode.com/problems/merge-nodes-in-between-zeros/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Problem_2181 {
+    public ListNode mergeNodes(ListNode head) {
+
+    }
+}
