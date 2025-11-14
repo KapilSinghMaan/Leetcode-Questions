64d862306a4bd00ebb594a44df291d648100d9c7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-14 20:17:37 +0530

    Linked List - Add two numbers

---
+++ Problem_2.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/add-two-numbers/description/
+
+// 2. Add Two Numbers
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Problem_2 {
+    
+}
