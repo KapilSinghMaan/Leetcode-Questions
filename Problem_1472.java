341bcb1cb60c60737256de0b87b9e88be56c7542
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-14 20:53:21 +0530

    Linked List - Design Browser History

--- Problem_1472.java
+++ Problem_1472.java
@@ -2,20 +2,48 @@
 
 // https://leetcode.com/problems/design-browser-history/description/
 
+class DLLNode {
+    public String data;
+    public DLLNode prev, next;
+
+    DLLNode(String url) {
+        data = url;
+        prev = null;
+        next = null;
+    }
+}
+
 public class Problem_1472 {
-    public BrowserHistory(String homepage) {
-        
+    private DLLNode linkedListHead;
+    private DLLNode current;
+
+    public Problem_1472(String homepage) {
+        linkedListHead = new DLLNode(homepage);
+        current=linkedListHead;
     }
 
     public void visit(String url) {
-
+        DLLNode newNode = new DLLNode(url);
+        current.next=newNode;
+        newNode.prev=current;
+        current=newNode;
     }
 
     public String back(int steps) {
+        while (steps>0 && current.prev!=null) {
+            current=current.prev;
+            steps--;
+        }
 
+        return current.data;
     }
 
     public String forward(int steps) {
+        while (steps>0&& current.next!=null) {
+            current=current.next;
+            steps--;
+        }
 
+        return current.data;
     }
 }
