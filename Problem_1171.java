0f0c70f4b42146e64cf6fcb36b111edf66b2631d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-14 11:35:04 +0530

    Remove Zero Sum Consecutive Nodes from Linked List

---
+++ Problem_1171.java
@@ -0,0 +1,26 @@
+// 1171. Remove Zero Sum Consecutive Nodes from Linked List
+
+// https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/description/ 
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Problem_1171 {
+     public ListNode removeZeroSumSublists(ListNode head) {
+        
+    }
+}
