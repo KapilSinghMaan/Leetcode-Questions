333fde597d2710e0cebc916697c3eb9ea4955c12
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-14 20:38:53 +0530

    Linked List - Design Browser History

---
+++ Problem_1472.java
@@ -0,0 +1,21 @@
+// 1472. Design Browser History
+
+// https://leetcode.com/problems/design-browser-history/description/
+
+public class Problem_1472 {
+    public BrowserHistory(String homepage) {
+        
+    }
+
+    public void visit(String url) {
+
+    }
+
+    public String back(int steps) {
+
+    }
+
+    public String forward(int steps) {
+
+    }
+}
