115a7b1f3591bc20cdb1a32fb9d955ffb1a2448b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-15 23:38:28 +0530

    Remove All Adjacent Duplicates in String

--- Problem_1047.java
+++ Problem_1047.java
@@ -2,9 +2,32 @@
 
 // https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
 
+import java.util.Stack;
+
 public class Problem_1047 {
 
     public String removeDuplicates(String s) {
-        
+     
+        Stack <Character> st =new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            if (!st.empty()) {
+                if (st.peek() == s.charAt(i)) {
+                    st.pop();
+                }else{
+                    st.push(s.charAt(i));
+                }
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+
+        StringBuilder ns=new StringBuilder();
+        while (!st.empty()) {
+            ns.append(st.pop());
+        }
+
+        return ns.reverse().toString();
     }
 }
