f5cacdff1cded4eb5aa2220b31e7227d054148af
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-18 09:35:50 +0530

    Simplify Path

--- Problem_71.java
+++ Problem_71.java
@@ -2,8 +2,34 @@
 
 // https://leetcode.com/problems/simplify-path/description/
 
+import java.util.Stack;
+
 class Problem_71 {
     public String simplifyPath(String path) {
+        
+        Stack<String> st = new Stack<>();
 
+        String[] components = path.split("/");
+
+        for (String comp : components) {
+            if (comp.equals(".") || comp.isEmpty()) {
+                continue;
+            } else if (comp.equals("..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } else {
+                st.add(comp);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (String dir : st) {
+            result.append("/");
+            result.append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
     }
 }
