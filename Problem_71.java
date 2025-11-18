f9793ac83f194dea52aeb25c7d8ffea247d9a3c3
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-18 09:21:18 +0530

    Simplify Path

--- Problem_71.java
+++ Problem_71.java
@@ -1,3 +1,9 @@
-class Problem_71
-{
+// 71. Simplify Path
+
+// https://leetcode.com/problems/simplify-path/description/
+
+class Problem_71 {
+    public String simplifyPath(String path) {
+
+    }
 }
