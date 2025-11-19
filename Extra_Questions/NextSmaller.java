45e4309a317928396fd5d43881fa389891f49776
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-19 10:36:55 +0530

    Next Smaller element in array

---
+++ Extra_Questions/NextSmaller.java
@@ -0,0 +1,12 @@
+// Next Smaller Element
+
+// Find the just smaller element on the right side of every element
+
+// input --> 4,5,2,25
+// output -> 2,2,-1,-1  (-1 if not next smaller)
+
+import java.util.Stack;
+
+public class NextSmaller {
+    
+}
