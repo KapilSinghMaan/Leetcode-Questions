4952ce0580e3c0d880e6f3a501c0417d2fc5fe3b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-19 10:37:14 +0530

    Next Smaller element in array

--- Extra_Questions/NextSmaller.java
+++ Extra_Questions/NextSmaller.java
@@ -8,5 +8,41 @@
 import java.util.Stack;
 
 public class NextSmaller {
-    
+    private static int[] nextSamller(int[] arr) {
+
+        int n = arr.length;
+
+        int[] output = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        st.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!st.isEmpty() && arr[i] <= arr[st.peek()]) {
+                output[st.peek()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        while (!st.isEmpty()) {
+            int top = st.peek();
+            output[top] = -1;
+            st.pop();
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 4, 5, 2, 25 };
+
+        int[] res = nextSamller(arr);
+
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
 }
