f9a4ea824509fac8d6c95a3e51fd23c802da11fc
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-19 22:36:45 +0530

    Largest Rectangle in Histogram

---
+++ Problem_84.java
@@ -0,0 +1,9 @@
+// 84. Largest Rectangle in Histogram
+
+// https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+
+public class Problem_84 {
+    public int largestRectangleArea(int[] heights) {
+        
+    }
+}
