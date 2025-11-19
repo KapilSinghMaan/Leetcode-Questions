bbaf2463482cbf69ebd1a5a96cd4422488992fbf
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-19 10:25:32 +0530

    Next Greater element in array

---
+++ Extra_Questions/NextGreater.java
@@ -0,0 +1,9 @@
+// Next Greater Element
+
+// Find the just greater element on the right side of every element
+
+public class NextGreater {
+    public static void main(String[] args) {
+        
+    }
+}
