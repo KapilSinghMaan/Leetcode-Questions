9bf6705f25b53d5a1653abba7e4068a92392e333
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-20 09:53:26 +0530

    Infix to Postfix Conversion

---
+++ Extra_Questions/InfixToPostfixConversion.java
@@ -0,0 +1,7 @@
+// Infix to Postfix Conversion
+
+// Infix to postfix conversion is the process of transforming an arithmetic expression where operators are placed between operands (infix) into a format where operators follow their operands (postfix), enabling easier computation by machines.
+
+public class InfixToPostfixConversion {
+    
+}
