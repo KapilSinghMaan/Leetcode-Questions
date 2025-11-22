b6bf38aab108f8f792829d1991c6e89b2b83fd37
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-22 12:10:10 +0530

    Evaluation of Postfix expression

---
+++ Extra_Questions/PostfixEvaluate.java
@@ -0,0 +1,14 @@
+// Evaluation of Postfix expression
+
+// postfix: 2536+**5/2-
+// result: 16
+
+import java.util.Stack;
+
+public class PostfixEvaluate {
+
+
+    public static void main(String[] args) {
+        System.out.println(postfixEvaluation("2536+**5/2-"));
+    }
+}
