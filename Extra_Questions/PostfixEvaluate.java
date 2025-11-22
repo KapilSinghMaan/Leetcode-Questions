92baeacac21ac4e2c627e4e5547c8ec104bd2e2a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-22 12:10:42 +0530

    Evaluation of Postfix expression

--- Extra_Questions/PostfixEvaluate.java
+++ Extra_Questions/PostfixEvaluate.java
@@ -7,6 +7,38 @@ import java.util.Stack;
 
 public class PostfixEvaluate {
 
+    public static int postfixEvaluation(String postfix) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < postfix.length(); i++) {
+            char ch = postfix.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');
+            } else {
+                int op2 = stack.pop();
+                int op1 = stack.pop();
+
+                switch (ch) {
+                    case '+':
+                        stack.push(op1 + op2);
+                        break;
+                    case '-':
+                        stack.push(op1 - op2);
+                        break;
+                    case '*':
+                        stack.push(op1 * op2);
+                        break;
+                    case '/':
+                        stack.push(op1 / op2);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
 
     public static void main(String[] args) {
         System.out.println(postfixEvaluation("2536+**5/2-"));
