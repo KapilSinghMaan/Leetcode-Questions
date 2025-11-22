091bc37554111013d4a57331e8b4be4c542c7db3
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-22 11:43:37 +0530

    Prefix To Postfix Conversion

---
+++ Extra_Questions/PrefixToPostfixConversion.java
@@ -0,0 +1,10 @@
+// Prefix to Postfix Conversion
+
+// Prefix to Postfix conversion means transforming an expression written in prefix notation (operator before operands) into postfix notation (operator after operands).
+
+// prefix: *+AB-CD
+// postfix: AB+CD-*
+
+public class PrefixToPostfixConversion {
+
+}
