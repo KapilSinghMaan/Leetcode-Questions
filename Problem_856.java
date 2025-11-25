a5bf1310a53da6efb097f238dc3697c62bcd0454
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-25 10:10:03 +0530

    Score of Parentheses

---
+++ Problem_856.java
@@ -0,0 +1,9 @@
+// 856. Score of Parentheses
+
+// https://leetcode.com/problems/score-of-parentheses/description/
+
+public class Problem_856 {
+     public int scoreOfParentheses(String s) {
+        
+    }
+}
