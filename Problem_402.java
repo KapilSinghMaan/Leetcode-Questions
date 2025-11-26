303a973a36697f4f4e0e7486a872b1fbc8e47279
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 09:40:49 +0530

    Remove K Digits

---
+++ Problem_402.java
@@ -0,0 +1,9 @@
+// 402. Remove K Digits
+
+// https://leetcode.com/problems/remove-k-digits/description/
+
+public class Problem_402 {
+    public String removeKdigits(String num, int k) {
+        
+    }
+}
