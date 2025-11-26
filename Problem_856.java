0e6c357b4df71e318f4fa90d1bc6b1de1fa3e0ce
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 09:36:52 +0530

    Score of Parentheses

--- Problem_856.java
+++ Problem_856.java
@@ -2,8 +2,22 @@
 
 // https://leetcode.com/problems/score-of-parentheses/description/
 
+import java.util.Stack;
+
 public class Problem_856 {
-     public int scoreOfParentheses(String s) {
-        
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int score = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(score);
+                score = 0;
+            } else {
+                score = stack.pop() + Math.max(2 * score, 1);
+            }
+        }
+
+        return score;
     }
 }
