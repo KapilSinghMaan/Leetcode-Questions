8a2109d2b51411312d54d6ddc1f2f43489f766d3
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 10:01:05 +0530

    Remove K Digits

--- Problem_402.java
+++ Problem_402.java
@@ -2,8 +2,39 @@
 
 // https://leetcode.com/problems/remove-k-digits/description/
 
+import java.util.Stack;
+
 public class Problem_402 {
     public String removeKdigits(String num, int k) {
-        
+        Stack<Character> stack=new Stack<>();
+
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k>0 && stack.peek()>digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+
+        for (int i = 0; i < k; i++) {
+            stack.pop();
+        }
+
+        StringBuilder ret=new StringBuilder();
+        boolean leadingZero=true;
+
+        for (char digit : stack) {
+            if (leadingZero && digit=='0') {
+                continue;
+            }
+            leadingZero=false;
+            ret.append(digit);
+        }
+
+        if (ret.length()==0) {
+            return "0";
+        }
+
+        return ret.toString();
     }
 }
