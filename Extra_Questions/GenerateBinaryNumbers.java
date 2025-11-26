d87784a48ade171e678fa35f3db05f5e7822ad45
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 21:54:52 +0530

    Generate Binary Numbers

---
+++ Extra_Questions/GenerateBinaryNumbers.java
@@ -0,0 +1,10 @@
+// Given a number 'n'. Write a program that generates and prints all binary numbers with decimal values from 1 to n.
+
+// Example : n = 5
+// Output : 1 , 10 , 11, 100 , 101
+
+public class GenerateBinaryNumbers {
+    public static void main(String[] args) {
+        
+    }
+}
