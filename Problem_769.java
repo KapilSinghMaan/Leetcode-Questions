b21baa7ea18a46052baf86dcff19877157b49227
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 20:44:35 +0530

    Max Chunks to make sorted

---
+++ Problem_769.java
@@ -0,0 +1,10 @@
+// 769. Max Chunks To Make Sorted
+
+// https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+
+public class Problem_769 {
+
+     public int maxChunksToSorted(int[] arr) {
+        
+    }
+}
