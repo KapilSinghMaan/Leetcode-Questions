0146d887c7e9133e9f170e6641eaca0a36215b51
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-26 21:01:41 +0530

    Max Chunks to make sorted

--- Problem_769.java
+++ Problem_769.java
@@ -5,6 +5,24 @@
 public class Problem_769 {
 
      public int maxChunksToSorted(int[] arr) {
-        
+        if(arr==null || arr.length==0) return 0;
+
+        int[] max=new int[arr.length];
+
+        max[0]=arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            max[i]=Math.max(max[i-1], arr[i]);
+        }
+
+        int count=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max[i]==i) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
