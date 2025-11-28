7761197da1f62e4726f68c487b40cf7932c64bac
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-28 12:01:08 +0530

    Construct Binary Search Tree from Preorder Traversal

---
+++ Problem_1008.java
@@ -0,0 +1,28 @@
+// 1008. Construct Binary Search Tree from Preorder Traversal
+
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/description/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_1008 {
+     public TreeNode bstFromPreorder(int[] preorder) {
+        
+    }
+}
