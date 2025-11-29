504c868c5deb80d74a444de6005572ced5b2be75
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-29 11:37:16 +0530

    Binary Search Tree to Greater Sum Tree

---
+++ Problem_1038.java
@@ -0,0 +1,29 @@
+// 1038. Binary Search Tree to Greater Sum Tree
+
+// https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_1038 {
+
+    public TreeNode bstToGst(TreeNode root) {
+        
+    }
+}
