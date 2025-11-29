512bef2c215041c847c7671eb951971ca3136f5b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-29 11:37:33 +0530

    Binary Search Tree to Greater Sum Tree

--- Problem_1038.java
+++ Problem_1038.java
@@ -22,8 +22,16 @@ class TreeNode {
 }
 
 public class Problem_1038 {
+    int pre = 0;
 
     public TreeNode bstToGst(TreeNode root) {
-        
+        if (root.right != null) {
+            bstToGst(root.right);
+        }
+        pre = root.val = pre + root.val;
+        if (root.left != null) {
+            bstToGst(root.left);
+        }
+        return root;
     }
 }
