304aff736473a6ff95a98852789cb8b982ddecc5
KapilSinghMaan <kapilmaan369@gmail.com>
2025-11-29 11:14:06 +0530

    Construct Binary Search Tree from Preorder Traversal

--- Problem_1008.java
+++ Problem_1008.java
@@ -22,7 +22,29 @@ class TreeNode {
 }
 
 public class Problem_1008 {
-     public TreeNode bstFromPreorder(int[] preorder) {
-        
+    int idx = 0;
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return helper(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public TreeNode helper(int[] preorder, int lower, int upper) {
+        if (idx == preorder.length) {
+            return null;
+        }
+
+        int val = preorder[idx];
+
+        if (val < lower || val > upper) {
+            return null;
+        }
+
+        idx++;
+        TreeNode root=new TreeNode(val);
+
+        root.left=helper(preorder, lower, val);
+        root.right=helper(preorder, val, upper);
+
+        return root;
     }
 }
