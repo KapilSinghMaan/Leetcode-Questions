124e61266bc5e233ca2f24388bfc513ac9fcde26
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-01 22:12:42 +0530

    Search in a Binary Search Tree

---
+++ Problem_700.java
@@ -0,0 +1,28 @@
+// 700. Search in a Binary Search Tree
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/ 
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_700 {
+ public TreeNode searchBST(TreeNode root, int val) {
+        
+    }   
+}
