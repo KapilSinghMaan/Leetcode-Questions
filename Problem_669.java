5482e88afc649b52df2255e65fddc95e26c73b8f
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-02 19:31:16 +0530

    Trim in a Binary Search Tree

---
+++ Problem_669.java
@@ -0,0 +1,28 @@
+// 669. Trim a Binary Search Tree
+
+// https://leetcode.com/problems/trim-a-binary-search-tree/description/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_669 {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        
+    }
+}
