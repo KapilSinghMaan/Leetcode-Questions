0215f1718ff4595132030d47afb6010effa6fe2e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-02 19:54:26 +0530

    Binary Tree Preorder Traversal

---
+++ Problem_144.java
@@ -0,0 +1,30 @@
+// 144. Binary Tree Preorder Traversal
+
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+    }
+}
