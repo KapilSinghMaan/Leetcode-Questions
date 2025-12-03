9761fa50db16706c4455563f9ee34ce48a088301
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 23:06:17 +0530

    Populating Next Right Pointers in Each Node II

--- Problem_117.java
+++ Problem_117.java
@@ -8,8 +8,9 @@ class Node {
     public Node right;
     public Node next;
 
-    public Node() {}
-    
+    public Node() {
+    }
+
     public Node(int _val) {
         val = _val;
     }
@@ -23,7 +24,36 @@ class Node {
 }
 
 public class Problem_117 {
-     public Node connect(Node root) {
-        
+    Node prev, leftMost;
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        this.leftMost = root;
+        Node curr = leftMost;
+
+        while (leftMost != null) {
+            this.prev = null;
+            curr = this.leftMost;
+            this.leftMost = null;
+            while (curr != null) {
+                processChild(curr.left);
+                processChild(curr.right);
+                curr = curr.next;
+            }
+        }
+        return root;
+    }
+
+    public void processChild(Node childNode) {
+        if (childNode != null) {
+            if (this.prev != null) {
+                this.prev.next = childNode;
+            } else {
+                this.leftMost = childNode;
+            }
+            this.prev = childNode;
+        }
     }
 }
