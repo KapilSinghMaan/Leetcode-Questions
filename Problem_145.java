c5ad5b54efd87b8e7c9d7ae9f5303990e50ffc91
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 09:52:35 +0530

    Binary Tree Postorder Traversal

---
+++ Problem_145.java
@@ -0,0 +1,32 @@
+// 145. Binary Tree Postorder Traversal
+
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+    }
+}
