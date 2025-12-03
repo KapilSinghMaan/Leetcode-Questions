5ec48f52ca8e7b7fd461765c6451e065f1ae48e7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 10:27:25 +0530

    Count Good Nodes in Binary Tree

---
+++ Problem_1448.java
@@ -0,0 +1,30 @@
+// 1448. Count Good Nodes in Binary Tree
+
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/description/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_1448 {
+    public int goodNodes(TreeNode root) {
+       
+    }
+
+   
+}
