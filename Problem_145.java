d7b990a7839e80067c62d693a4ece225eb2acd83
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 09:53:01 +0530

    Binary Tree Postorder Traversal

--- Problem_145.java
+++ Problem_145.java
@@ -27,6 +27,32 @@ class TreeNode {
 
 public class Problem_145 {
     public List<Integer> postorderTraversal(TreeNode root) {
-        
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode currNode = stack.peek();
+
+            if ((currNode.left == null && currNode.right == null)
+                    || (prev != null && (prev == currNode.left || prev == currNode.right))) {
+                stack.pop();
+                result.add(currNode.val);
+                prev = currNode;
+            } else {
+                if (currNode.right != null) {
+                    stack.push(currNode.right);
+                }
+                if (currNode.left != null) {
+                    stack.push(currNode.left);
+                }
+            }
+        }
+        return result;
     }
 }
