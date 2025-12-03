18fc9b3c38a65086aa2d7e6402c049e8783476ac
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 10:49:00 +0530

    Minimum Absolute Difference in BST

---
+++ Problem_530.java
@@ -0,0 +1,28 @@
+// 530. Minimum Absolute Difference in BST
+
+// https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Problem_530 {
+    public int getMinimumDifference(TreeNode root) {
+               
+    }
+}
