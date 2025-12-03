e648e1dfc93bdb5b395c8bb06eb29454ac12e0f4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-03 10:09:15 +0530

    Binary Tree Inorder Traversal

--- Problem_94.java
+++ Problem_94.java
@@ -2,7 +2,9 @@
 
 // https://leetcode.com/problems/binary-tree-inorder-traversal/description/
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class TreeNode {
     int val;
@@ -25,6 +27,25 @@ class TreeNode {
 
 public class Problem_94 {
     public List<Integer> inorderTraversal(TreeNode root) {
-               
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
     }
 }
