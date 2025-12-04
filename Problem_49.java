87f76b7a1945a2690bf0614e2c1ea5487600ede7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 11:51:10 +0530

    Group Anagrams

---
+++ Problem_49.java
@@ -0,0 +1,11 @@
+// 49. Group Anagrams
+
+// https://leetcode.com/problems/group-anagrams/description/
+
+import java.util.List;
+
+public class Problem_49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+    }
+}
