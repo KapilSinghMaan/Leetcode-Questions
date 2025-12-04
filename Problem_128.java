1edd33833f63be7c6ae2959eaf4461136ae93d07
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 23:27:25 +0530

    Longest Consecutive Sequence

---
+++ Problem_128.java
@@ -0,0 +1,11 @@
+// 128. Longest Consecutive Sequence
+
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+import java.util.HashMap;
+
+public class Problem_128 {
+    public int longestConsecutive(int[] nums) {
+
+    }
+}
