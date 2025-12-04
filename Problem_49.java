b5bf2e5fec9d7c29b0c0a45c3838bac5731bdd93
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 19:16:18 +0530

    Group Anagrams

--- Problem_49.java
+++ Problem_49.java
@@ -2,10 +2,23 @@
 
 // https://leetcode.com/problems/group-anagrams/description/
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Problem_49 {
     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
         
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
     }
 }
