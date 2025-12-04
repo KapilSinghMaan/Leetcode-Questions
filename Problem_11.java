ae6ea20620a4c48aa838e1c1b25f750e81792370
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 10:48:01 +0530

    Container With Most Water

---
+++ Problem_11.java
@@ -0,0 +1,9 @@
+// 11. Container With Most Water
+
+// https://leetcode.com/problems/container-with-most-water/description/
+
+public class Problem_11 {
+    public int maxArea(int[] height) {
+        
+    }
+}
