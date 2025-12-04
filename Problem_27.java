1f42caa9a220cfd3e9fab32ec1e3b09397c5dd1b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 11:44:41 +0530

    Remove Element

---
+++ Problem_27.java
@@ -0,0 +1,9 @@
+// 27. Remove Element
+
+// https://leetcode.com/problems/remove-element/description/
+
+public class Problem_27 {
+     public int removeElement(int[] nums, int val) {
+        
+    }
+}
