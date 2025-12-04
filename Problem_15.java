7c8d6f0e12b82f688d016b677370e5440428477e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 11:21:01 +0530

    3Sum

---
+++ Problem_15.java
@@ -0,0 +1,12 @@
+// 15. 3Sum
+
+// https://leetcode.com/problems/3sum/description/
+
+import java.util.List;
+
+public class Problem_15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    }
+}
