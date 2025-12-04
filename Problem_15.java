35e89ad2440a7913893020a09dd807320a663747
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 11:32:34 +0530

    3Sum

--- Problem_15.java
+++ Problem_15.java
@@ -2,11 +2,45 @@
 
 // https://leetcode.com/problems/3sum/description/
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem_15 {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    int leftVal = nums[left];
+                    int rightVal = nums[right];
+                    while (left < right && nums[left] == leftVal) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == rightVal) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 }
