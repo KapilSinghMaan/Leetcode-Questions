c66f57fe3d43102ca175a81ee79ed74e45b7d36c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 23:27:59 +0530

    Longest Consecutive Sequence

--- Problem_128.java
+++ Problem_128.java
@@ -7,5 +7,31 @@ import java.util.HashMap;
 public class Problem_128 {
     public int longestConsecutive(int[] nums) {
 
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], true);
+        }
+
+        for (int num : nums) {
+            if (map.containsKey(num - 1)) {
+                map.put(num, false);
+            }
+        }
+        int maxLength = 0;
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == true) {
+                int currentLength = 1;
+                int currentValue = key + 1;
+                while (map.containsKey(currentValue)) {
+                    currentLength++;
+                    currentValue++;
+                }
+                maxLength = maxLength > currentLength ? maxLength : currentLength;
+            }
+        }
+
+        return maxLength;
     }
 }
