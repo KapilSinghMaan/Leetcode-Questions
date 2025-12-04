1bf20349e436689984123809cb188df8303b412a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-04 22:25:01 +0530

    Check Pair Sum mod K is true/false

---
+++ Extra_Questions/Check_PairSum_Mod_K.java
@@ -0,0 +1,11 @@
+// Given a integer array and a number 'k', return true/false if the given array can be divided into pairs such that the sum of every pair is divisible by 'k'.
+
+// Example : array = [20, 25, 10, 3, 15, 27] , k = 10
+// Output : true
+
+public class Check_PairSum_Mod_K {
+
+    public static void main(String[] args) {
+
+    }
+}
