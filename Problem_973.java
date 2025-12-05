46eb2c11b3cac593fc3703e0bcf67057b1d35200
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-06 00:40:15 +0530

    K Closest Points to Origin

---
+++ Problem_973.java
@@ -0,0 +1,9 @@
+// 973. K Closest Points to Origin
+
+// https://leetcode.com/problems/k-closest-points-to-origin/description/
+
+public class Problem_973 {
+    public int[][] kClosest(int[][] points, int k) {
+        
+    }
+}
