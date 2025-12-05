6101704c90170562d93eb2f2c7884dbf93627200
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-06 01:05:39 +0530

    Maximum Difference Between Two Subsets of m Elements

---
+++ Extra_Questions/MaxDiff_BW_Subset_Of_M_Ele.java
@@ -0,0 +1,19 @@
+// Maximum Difference Between Two Subsets of m Elements
+
+// Given an array of n integers and a number m, find the maximum possible difference between two sets of m elements chosen from given array.
+
+// Input : arr[] = { 1, 2, 3, 4, 5 } , m = 4
+// Output : 4
+
+// Explanation : The maximum four elements are 2 3 4 5 and the minimum four elements are 1 2 3 4. The difference between two subsets sum is (2 + 3 + 4 + 5) - (1 + 2 + 3 + 4) = 4
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaxDiff_BW_Subset_Of_M_Ele {
+
+    
+    public static void main(String[] args) {
+    
+    }
+}
