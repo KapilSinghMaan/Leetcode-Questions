1099992b45b634c2a6a69bf313028d0431e9c758
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-06 00:20:30 +0530

    Connect n ropes with minimum cost

---
+++ Extra_Questions/Connect_N_Ropes_With_minCost.java
@@ -0,0 +1,14 @@
+// Connect n ropes with minimum cost
+
+// Given are N ropes of different lengths, the task is to connect these ropes into one rope with minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
+
+// Input : arr[] = {4, 3, 2, 6} , N = 4
+// Output : 29
+
+// Explanation : First, connect ropes of length 2 and 3. Now we have three ropes of lengths 4, 6, 5. Now connect ropes of lengths 4 add 5. Now we have two ropes of lengths 6 and 9. Finally connect the two ropes and all ropes have connected.
+
+public class Connect_N_Ropes_With_minCost {
+    public static void main(String[] args) {
+        
+    }
+}
