442533d2da87ec5a31ca023128e746f20efb9da7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-05 19:49:00 +0530

    K-th Smallest Prime Fraction

---
+++ Problem_786.java
@@ -0,0 +1,9 @@
+// 786. K-th Smallest Prime Fraction
+
+// https://leetcode.com/problems/k-th-smallest-prime-fraction/description/
+
+public class Problem_786 {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        
+    }
+}
