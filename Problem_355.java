3659753ab47fd451c51434010f40c32b40fb126d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-05 21:50:00 +0530

    Design Twitter

---
+++ Problem_355.java
@@ -0,0 +1,28 @@
+// 355. Design Twitter
+
+// https://leetcode.com/problems/design-twitter/description/
+
+import java.util.List;
+
+public class Problem_355 {
+    public Problem_355() // constructor name same as class name
+    {
+
+    }
+
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+
+    }
+
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+
+    }
+}
