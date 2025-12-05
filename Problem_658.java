9dd8a5473f6d50cb308702a893f678b6c8ae3cd8
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-05 19:07:33 +0530

    Find K Closest Elements

---
+++ Problem_658.java
@@ -0,0 +1,11 @@
+// 658. Find K Closest Elements
+
+// https://leetcode.com/problems/find-k-closest-elements/description/
+
+import java.util.List;
+
+public class Problem_658 {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+    }
+}
