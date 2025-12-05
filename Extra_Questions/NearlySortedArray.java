a4bf67374d3823c4baaf43e0d1177a5ee29b977e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-05 18:58:45 +0530

    Sort a Nearly Sorted Array

--- Extra_Questions/NearlySortedArray.java
+++ Extra_Questions/NearlySortedArray.java
@@ -8,9 +8,36 @@
 // Input : arr[] = {10, 9, 8, 7, 4, 70, 60, 50} K = 4
 // Output : arr[] = { 4, 7, 8, 9, 10, 50, 60, 70}
 
+import java.util.PriorityQueue;
+
 public class NearlySortedArray {
 
+    public static int[] nearlySorted(int[] arr, int k) {
+
+        int[] res = new int[arr.length];
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int i = 0;
+        for (int num : arr) {
+            heap.add(num);
+            if (heap.size() > k) {
+                res[i++] = heap.poll();
+            }
+        }
+        while (!heap.isEmpty()) {
+            res[i++] = heap.poll();
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        
+
+        int[] arr = { 10, 9, 8, 7, 4, 70, 60, 50 };
+
+        int[] res = nearlySorted(arr, 4);
+
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
     }
 }
