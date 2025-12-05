d633cdd9232a0219db583936398ec4d4527f275c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-05 18:19:24 +0530

    Sort a Nearly Sorted Array

---
+++ Extra_Questions/NearlySortedArray.java
@@ -0,0 +1,16 @@
+// Sort a nearly sorted (or K sorted) array
+
+// Given an array of N elements, where each element is at most K away from its target position, devise an algorithm that sorts in O(N log K) time.
+
+// Input : arr[]= {6, 5, 3, 2, 8, 10, 9} K = 3
+// Output : arr[] = {2, 3, 5, 6, 8, 9, 10}
+
+// Input : arr[] = {10, 9, 8, 7, 4, 70, 60, 50} K = 4
+// Output : arr[] = { 4, 7, 8, 9, 10, 50, 60, 70}
+
+public class NearlySortedArray {
+
+    public static void main(String[] args) {
+        
+    }
+}
