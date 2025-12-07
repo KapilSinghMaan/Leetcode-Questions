e096bf50c079d06f5143c157ef27f0a6ca189e33
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-07 17:27:35 +0530

    Maximum Units on a Truck

---
+++ Problem_1710.java
@@ -0,0 +1,9 @@
+// 1710. Maximum Units on a Truck
+
+// https://leetcode.com/problems/maximum-units-on-a-truck/description/
+
+public class Problem_1710 {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        
+    }
+}
