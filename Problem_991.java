f176abff8a5be5c7349855b18b32346144d3249d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-07 16:46:10 +0530

    Broken Calculator

--- Problem_991.java
+++ Problem_991.java
@@ -4,6 +4,16 @@
 
 public class Problem_991 {
     public int brokenCalc(int startValue, int target) {
-        
+        int ans = 0;
+        while (target > startValue) {
+            if (target % 2 == 0) {
+                target /= 2;
+            } else {
+                target++;
+            }
+            ans++;
+        }
+        ans += startValue - target;
+        return ans;
     }
 }
