105a50daf35553ec4ea644a5e17d92708954205c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-07 16:43:55 +0530

    Broken Calculator

---
+++ Problem_991.java
@@ -0,0 +1,9 @@
+// 991. Broken Calculator
+
+// https://leetcode.com/problems/broken-calculator/description/
+
+public class Problem_991 {
+    public int brokenCalc(int startValue, int target) {
+        
+    }
+}
