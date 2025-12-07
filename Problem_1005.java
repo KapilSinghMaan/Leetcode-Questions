ee9602601335c63d9acb89ac8e735118e9107d90
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-07 16:39:07 +0530

    Maximize Sum Of Array After K Negations

--- Problem_1005.java
+++ Problem_1005.java
@@ -2,8 +2,27 @@
 
 // https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/description/
 
+import java.util.PriorityQueue;
+
 class Problem_1005 {
     public int largestSumAfterKNegations(int[] nums, int k) {
 
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            heap.add(num);
+        }
+
+        while (k-- > 0) {
+            int x = heap.poll();
+            x *= -1;
+            heap.add(x);
+        }
+        int sum = 0;
+        while (!heap.isEmpty()) {
+            sum += heap.poll();
+        }
+
+        return sum;
     }
 }
