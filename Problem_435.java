642a608400e2c7cd56a7415273f94a0f519538d6
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-07 17:14:49 +0530

    Non-overlapping Intervals

--- Problem_435.java
+++ Problem_435.java
@@ -2,8 +2,24 @@
 
 // https://leetcode.com/problems/non-overlapping-intervals/description/
 
+import java.util.Arrays;
+
 public class Problem_435 {
     public int eraseOverlapIntervals(int[][] intervals) {
-        
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int end = intervals[0][1];
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= end) {
+                end = intervals[i][1];
+            } else {
+                ans++;
+                if (intervals[i][1] <= end) {
+                    end = intervals[i][1];
+                }
+            }
+        }
+        return ans;
     }
 }
