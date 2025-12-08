a553c7d91f3f55ec060eebac2037c350c3ff88c3
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-08 20:50:15 +0530

    Keys and Rooms

---
+++ Problem_841.java
@@ -0,0 +1,11 @@
+// 841. Keys and Rooms
+
+// https://leetcode.com/problems/keys-and-rooms/description/
+
+import java.util.List;
+
+public class Problem_841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        
+    }
+}
