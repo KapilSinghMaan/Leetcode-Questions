f442dacbe34e2e3aab7670e6fad1d46a4a237ceb
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-08 16:02:51 +0530

    Merge Intervals

---
+++ Problem_56.java
@@ -0,0 +1,9 @@
+// 56. Merge Intervals
+
+// https://leetcode.com/problems/merge-intervals/description/
+
+public class Problem_56 {
+    public int[][] merge(int[][] intervals) {
+        
+    }
+}
