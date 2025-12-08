9bb122724a9f588d25cb5a519d4489a02118287a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-08 20:54:17 +0530

    Keys and Rooms

--- Problem_841.java
+++ Problem_841.java
@@ -3,9 +3,31 @@
 // https://leetcode.com/problems/keys-and-rooms/description/
 
 import java.util.List;
+import java.util.Stack;
 
 public class Problem_841 {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        
+        boolean[] visited = new boolean[rooms.size()];
+        visited[0] = true;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        while (!stack.isEmpty()) {
+            int curr = stack.pop();
+
+            for (int next : rooms.get(curr)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    stack.push(next);
+                }
+            }
+        }
+
+        for (boolean room : visited) {
+            if (!room) {
+                return false;
+            }
+        }
+        return true;
     }
 }
