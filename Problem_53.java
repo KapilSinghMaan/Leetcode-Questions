bf5cabc248f90c4a76269c1419536770047744e1
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-08 15:56:48 +0530

    Maximum Subarray

--- Problem_53.java
+++ Problem_53.java
@@ -2,8 +2,16 @@
 
 // https://leetcode.com/problems/maximum-subarray/description/
 
-class Problem_53{
-     public int maxSubArray(int[] nums) {
-        
+class Problem_53 {
+    public int maxSubArray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
     }
 }
