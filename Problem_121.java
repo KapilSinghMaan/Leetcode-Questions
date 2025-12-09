233dae87162253446a7ed64ad6c983d0fbd8c159
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-09 10:40:24 +0530

    Best Time to Buy and Sell Stock

--- Problem_121.java
+++ Problem_121.java
@@ -4,6 +4,17 @@
 
 public class Problem_121 {
     public int maxProfit(int[] prices) {
-        
+        int maxProfit = 0;
+        int low = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            int priceToday = prices[i] - low;
+            if (priceToday > maxProfit) {
+                maxProfit = priceToday;
+            }
+            if (prices[i] < low) {
+                low = prices[i];
+            }
+        }
+        return maxProfit;
     }
 }
