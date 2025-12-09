4b143c034e11eb2ba232294bb619fdc6ffe47da0
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-09 10:39:58 +0530

    Best Time to Buy and Sell Stock

---
+++ Problem_121.java
@@ -0,0 +1,9 @@
+// 121. Best Time to Buy and Sell Stock
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+public class Problem_121 {
+    public int maxProfit(int[] prices) {
+        
+    }
+}
