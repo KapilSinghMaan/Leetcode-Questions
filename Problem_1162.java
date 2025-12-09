e35907538cdffddc1c8a467409befab3d84fef01
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-09 20:02:41 +0530

    As Far from Land as Possible

---
+++ Problem_1162.java
@@ -0,0 +1,9 @@
+// 1162. As Far from Land as Possible
+
+// https://leetcode.com/problems/as-far-from-land-as-possible/description/
+
+public class Problem_1162 {
+     public int maxDistance(int[][] grid) {
+        
+    }
+}
