7d0e4a9ef4bb3dff9bb22f903e4690b70dd12132
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-09 09:32:23 +0530

    Rotting Oranges

---
+++ Problem_994.java
@@ -0,0 +1,9 @@
+// 994. Rotting Oranges
+
+// https://leetcode.com/problems/rotting-oranges/description/
+
+public class Problem_994 {
+     public int orangesRotting(int[][] grid) {
+        
+    }
+}
