dc4509f7a8f4391dc8c4c8c69a13ff8c8c7a1319
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 01:02:00 +0530

    Course Schedule II

--- Problem_210.java
+++ Problem_210.java
@@ -2,8 +2,50 @@
 
 // https://leetcode.com/problems/course-schedule-ii/description/
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Problem_210 {
-     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int pre = edge[1];
+            graph.get(pre).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int[] result = new int[numCourses];
+        int idx = 0;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            result[idx++] = curr;
+
+            for (int next : graph.get(curr)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        return idx == numCourses ? result : new int[0];
     }
 }
