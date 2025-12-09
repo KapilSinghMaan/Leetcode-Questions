69aa951b67696719687293559f6f86188e9ad3a7
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 00:28:49 +0530

    Course Schedule

---
+++ Problem_207.java
@@ -0,0 +1,9 @@
+// 207. Course Schedule
+
+// https://leetcode.com/problems/course-schedule/description/
+
+public class Problem_207 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    }
+}
