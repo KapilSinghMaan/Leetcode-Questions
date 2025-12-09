506a1263c16f679c714db4ba8b9d5217198907d4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-09 19:34:02 +0530

    Shortest Bridge

---
+++ Problem_934.java
@@ -0,0 +1,9 @@
+// 934. Shortest Bridge
+
+// https://leetcode.com/problems/shortest-bridge/description/
+
+public class Problem_934 {
+    public int shortestBridge(int[][] grid) {
+        
+    }
+}
