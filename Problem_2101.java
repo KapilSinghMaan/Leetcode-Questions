34e59b696ccb3e93a2379ae4288068243fa6e7d3
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 20:29:19 +0530

    Detonate the Maximum Bombs

---
+++ Problem_2101.java
@@ -0,0 +1,12 @@
+// 2101. Detonate the Maximum Bombs
+
+// https://leetcode.com/problems/detonate-the-maximum-bombs/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_2101 {
+    public int maximumDetonation(int[][] bombs) {
+        
+    }
+}
