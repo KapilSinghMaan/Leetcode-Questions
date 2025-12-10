869a0d49b77790436b63480a4da0b72b28917c23
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 20:34:30 +0530

    Detonate the Maximum Bombs

--- Problem_2101.java
+++ Problem_2101.java
@@ -6,7 +6,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem_2101 {
+
+    public int dfs(List<List<Integer>> graph, int node, boolean[] visited) {
+        visited[node] = true;
+
+        int count = 1;
+        for (int neighbour : graph.get(node)) {
+            if (!visited[neighbour]) {
+                count += dfs(graph, neighbour, visited);
+            }
+        }
+        return count;
+    }
+
     public int maximumDetonation(int[][] bombs) {
-        
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < bombs.length; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < bombs.length; i++) {
+            long x = bombs[i][0];
+            long y = bombs[i][1];
+            long r = bombs[i][2];
+
+            for (int j = 0; j < bombs.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                long dx = x - bombs[j][0];
+                long dy = y - bombs[j][1];
+
+                long distance = dx * dx + dy * dy;
+
+                if (distance <= r * r) {
+                    graph.get(i).add(j);
+                }
+            }
+        }
+
+        int max = 1;
+
+        for (int i = 0; i < bombs.length; i++) {
+            boolean[] visited = new boolean[bombs.length];
+            int count = dfs(graph, i, visited);
+            max = Math.max(count, max);
+        }
+
+        return max;
     }
 }
