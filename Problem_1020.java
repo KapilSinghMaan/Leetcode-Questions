1c09b0717a8a58f81062a897d37b1fb0fec37828
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 19:42:07 +0530

    Number of Enclaves

---
+++ Problem_1020.java
@@ -0,0 +1,10 @@
+// 1020. Number of Enclaves
+
+// https://leetcode.com/problems/number-of-enclaves/description/
+
+public class Problem_1020 {
+
+     public int numEnclaves(int[][] grid) {
+        
+    }
+}
