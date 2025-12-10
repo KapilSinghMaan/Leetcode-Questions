00ca3614787aa187bbf02d4ef235d1ddc7320e6c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-10 20:22:41 +0530

    Number of Enclaves

--- Problem_1020.java
+++ Problem_1020.java
@@ -4,7 +4,53 @@
 
 public class Problem_1020 {
 
-     public int numEnclaves(int[][] grid) {
-        
+    int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    public void dfs(int[][] grid, int r, int c, int n, int m) {
+        if (r < 0 || r >= n || c < 0 || c >= m || grid[r][c] != 1) {
+            return;
+        }
+        grid[r][c] = 0;
+        for (int[] d : dirs) {
+            dfs(grid, r + d[0], c + d[1], n, m);
+        }
+    }
+
+    public int numEnclaves(int[][] grid) {
+
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0, n, m);
+            }
+            if (grid[i][m - 1] == 1) {
+                dfs(grid, i, m - 1, n, m);
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (grid[0][i] == 1) {
+                dfs(grid, 0, i, n, m);
+            }
+            if (grid[n - 1][i] == 1) {
+                dfs(grid, n - 1, i, n, m);
+            }
+        }
+
+        int enclaves = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    enclaves++;
+                }
+            }
+        }
+
+        return enclaves;
     }
 }
