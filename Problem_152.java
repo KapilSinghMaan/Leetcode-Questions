b2ffb65a9aaf3a3f8e07c6ac6da6bce6e0703a94
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-18 23:44:34 +0530

    Maximum Product Subarray

---
+++ Problem_152.java
@@ -0,0 +1,10 @@
+// 152. Maximum Product Subarray
+
+// https://leetcode.com/problems/maximum-product-subarray/description/
+
+public class Problem_152 {
+    
+    public int maxProduct(int[] nums) {
+        
+    }
+}
