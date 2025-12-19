fa11ff22af7958ac9edf9feba24e758f9fb40eab
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-19 22:54:42 +0530

    Maximum Product Subarray

--- Problem_152.java
+++ Problem_152.java
@@ -5,6 +5,25 @@
 public class Problem_152 {
     
     public int maxProduct(int[] nums) {
-        
+         int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+
+            if (curr < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+
+            maxProd = Math.max(curr, curr * maxProd);
+            minProd = Math.min(curr, curr * minProd);
+
+            ans = Math.max(ans, maxProd);
+        }
+
+        return ans;
     }
 }
