a2e0ecb97e6a865f99cdb220f90322fec771d0fd
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-22 23:11:41 +0530

    Rotate Array

--- Problem_189.java
+++ Problem_189.java
@@ -3,7 +3,21 @@
 // https://leetcode.com/problems/rotate-array/description/
 
 public class Problem_189 {
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-        System.out.println("tomorrow");
+         int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 }
