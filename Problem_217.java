a27283e70eb8d1bfd15eb44a5cc83eb93a488d46
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-24 22:55:04 +0530

    Contains Duplicate

---
+++ Problem_217.java
@@ -0,0 +1,10 @@
+// 217. Contains Duplicate
+
+// https://leetcode.com/problems/contains-duplicate/description/
+
+public class Problem_217 {
+    
+    public boolean containsDuplicate(int[] nums) {
+        
+    }
+}
