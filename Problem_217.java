a58533e5ba9fce490b3a8fb8d62edcb1412db211
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-25 22:02:45 +0530

    Contains Duplicate

--- Problem_217.java
+++ Problem_217.java
@@ -2,9 +2,20 @@
 
 // https://leetcode.com/problems/contains-duplicate/description/
 
+import java.util.HashSet;
+
 public class Problem_217 {
     
     public boolean containsDuplicate(int[] nums) {
-        
+         HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (set.contains(n)) {
+                return true;
+            }
+            set.add(n);
+        }
+
+        return false;
     }
 }
