9cf8db6a731f9d3a2f23d3ddbdbe1eb65166cc67
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-26 12:54:23 +0530

    Valid Anagram

---
+++ Problem_242.java
@@ -0,0 +1,9 @@
+// 242. Valid Anagram
+
+// https://leetcode.com/problems/valid-anagram/description/
+
+public class Problem_242 {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+}
