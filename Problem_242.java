9e8dae95f91a84bfaafbffa27333eb186e9aff6c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-12-28 23:19:47 +0530

    Valid Anagram

--- Problem_242.java
+++ Problem_242.java
@@ -4,6 +4,18 @@
 
 public class Problem_242 {
     public boolean isAnagram(String s, String t) {
+       if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray())
+            count[ch - 'a']++;
+
+        for (char ch : t.toCharArray())
+            count[ch - 'a']--;
+
+        for (int c : count)
+            if (c != 0) return false;
         return true;
     }
 }
