9f32d0bfe2cab0c267cf92715e2d5c4b6eb00bf4
KapilSinghMaan <kapilmaan369@gmail.com>
2026-01-01 21:38:28 +0530

    Valid Anagram

--- Problem_242.java
+++ Problem_242.java
@@ -4,21 +4,22 @@
 
 public class Problem_242 {
     public boolean isAnagram(String s, String t) {
-       if (s.length() != t.length()) return false;
+        if (s.length() != t.length())
+            return false;
 
         int[] count = new int[26];
 
-        for (char ch : s.toCharArray()){
+        for (char ch : s.toCharArray()) {
             count[ch - 'a']++;
         }
 
-        for (char ch : t.toCharArray())
-        {
+        for (char ch : t.toCharArray()) {
             count[ch - 'a']--;
         }
 
-        for (int c : count){
-            if (c != 0) return false;
+        for (int c : count) {
+            if (c != 0)
+                return false;
         }
         return true;
     }
