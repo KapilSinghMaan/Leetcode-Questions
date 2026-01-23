73aa68733ff63f5058c5e7316c7a684d9a42077d
KapilSinghMaan <kapilmaan369@gmail.com>
2026-01-23 23:20:13 +0530

    Missing Number

--- Problem_268.java
+++ Problem_268.java
@@ -10,7 +10,7 @@ public class Problem_268 {
         int n = nums.length;
         for (int i = 0; i <= n; i++)
         {
-            xor ^= i;
+            xor ^= i; 
         }
         for (int num : nums)
         {
