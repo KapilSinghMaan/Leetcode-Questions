332d5e8558cc417e8c3763e6ee2cf619ccf51150
KapilSinghMaan <kapilmaan369@gmail.com>
2026-01-29 21:55:10 +0530

    Missing Number

--- Problem_268.java
+++ Problem_268.java
@@ -3,18 +3,17 @@
 // https://leetcode.com/problems/missing-number/description/
 
 public class Problem_268 {
-
     public int missingNumber(int[] nums) 
     {
         int xor = 0;
         int n = nums.length;
         for (int i = 0; i <= n; i++)
         {
-            xor ^= i; 
+            xor ^= i;
         }
         for (int num : nums)
         {
-            xor ^= num;
+            xor ^= num; 
         }
         return xor;
     }
